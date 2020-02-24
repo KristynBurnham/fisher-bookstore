1515cef3da283869140b48626170980b2dc9403d
KristynBurnham <kristynburnham@hotmail.com>
2020-02-24 11:12:42 -0500

    init the project

--- src/books/Books.js
+++ src/books/Books.js
@@ -1,36 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Books.css";
+import { BookDisplay } from "./BookDisplay";
 
-export class Books {
-    constructor() {
-        render();
-        {
-            return (<div className="Books">
+export default class Books extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: [
+                {
+                    id: 1,
+                    title: "Domain Driven Design",
+                    author: "Eric Evans",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id: 2,
+                    title: "Accelerate",
+                    author: "Nicole Forsgren",
+                    isbn: "978-1942788331"
+                },
+                {
+                    id: 3,
+                    title: "Cassandra's Story",
+                    author: "Kristyn Burnham",
+                    isbn: "978-1234567890"
+                },
+                {
+                    id: 4,
+                    title: "Henry's Story",
+                    author: "Kristyn Burnham",
+                    isbn: "978-1234567891"
+                },
+                {
+                    id: 5,
+                    title: "Joshua's Story",
+                    author: "Kristyn Burnham",
+                    isbn: "978-1234567892"    
+                },
+                {
+                    id: 6,
+                    title: "Xavier's Story",
+                    author: "Kristyn Burnham",
+                    isbn: "978-1234567893"
+                },
+                {
+                    id: 7,
+                    title: "Last One",
+                    author: "Kristyn Burnham",
+                    isbn: "978-1234567894"    
+                }
+            ]
+        };
+    } 
+    
+    render() {
+        return (
+            <div className="Books">
                 <div className="lander">
                     <BookDisplay books={this.state.books} />
                 </div>
-            </div>);
-        }
-        ;
-        constructor(props);
-        {
-            super(props);
-            this.state = {
-                books: [
-                    {
-                        id: 1,
-                        title: "Domain Driven Design",
-                        author: "Eric Evans",
-                        isbn: "978-0321125217"
-                    },
-                    {
-                        id: 2,
-                        title: "Accelerate",
-                        author: "Nicole Forsgren",
-                        isbn: "978-1942788331"
-                    }
-                ]
-            };
-        }
+            </div>
+        );
     }
 }

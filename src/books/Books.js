5f05cb5683dd767eab2127f4bac8bd4304bc1a25
KristynBurnham <kristynburnham@hotmail.com>
2020-02-24 10:06:10 -0500

    init the project

--- src/books/Books.js
+++ src/books/Books.js
@@ -1,13 +1,36 @@
 import React from "react";
 import "./Books.css";
 
-export default function Books() {
-    return (
-        <div className="Books">
-            <div className="lander">
-                <h1>Books</h1>
-                <p>Books</p>
-            </div>
-        </div>
-    );
+export class Books {
+    constructor() {
+        render();
+        {
+            return (<div className="Books">
+                <div className="lander">
+                    <BookDisplay books={this.state.books} />
+                </div>
+            </div>);
+        }
+        ;
+        constructor(props);
+        {
+            super(props);
+            this.state = {
+                books: [
+                    {
+                        id: 1,
+                        title: "Domain Driven Design",
+                        author: "Eric Evans",
+                        isbn: "978-0321125217"
+                    },
+                    {
+                        id: 2,
+                        title: "Accelerate",
+                        author: "Nicole Forsgren",
+                        isbn: "978-1942788331"
+                    }
+                ]
+            };
+        }
+    }
 }

3ec0262db065528d087c0b35f8c56168aa9e667c
KristynBurnham <kristynburnham@hotmail.com>
2020-02-26 15:29:10 -0500

    init the project

--- src/authors/Authors.js
+++ src/authors/Authors.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Authors.css";
+import { AuthorDisplay } from "./AuthorDisplay";
 
-export default function Authors() {
-    return (
-        <div className="Authors">
-            <div className="lander">
-                <h1>Authors</h1>
-                <p>Authors</p>
+export default class Authors extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            authors: [
+                {
+                    id: 1,
+                    author: "Eric Evans",
+                    title: "SciFi"
+                },
+                {
+                    id: 2,
+                    author: "Nicole Forsgren",
+                    title: "Horror"
+                },
+                {
+                    id: 3,
+                    author: "Kristyn Burnham",
+                    title: "Fictional"
+                }
+            ]
+        };
+    } 
+    
+    render() {
+        return (
+            <div className="Authors">
+                <div className="lander">
+                    <AuthorDisplay authors={this.state.authors} />
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }

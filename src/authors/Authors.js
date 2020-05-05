ca392fd385ac36a779801fc38f533f6d87a59f92
KristynBurnham <kristynburnham@hotmail.com>
2020-05-05 12:23:00 -0400

    please commit

--- src/authors/Authors.js
+++ src/authors/Authors.js
@@ -1,38 +1,19 @@
-import React, { Component } from "react";
+import React, {useEffect, useState} from "react";
 import "./Authors.css";
 import { AuthorDisplay } from "./AuthorDisplay";
 
-export default class Authors extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            authors: [
-                {
-                    id: 1,
-                    author: "Eric Evans",
-                    title: "SciFi"
-                },
-                {
-                    id: 2,
-                    author: "Nicole Forsgren",
-                    title: "Horror"
-                },
-                {
-                    id: 3,
-                    author: "Kristyn Burnham",
-                    title: "Fictional"
-                }
-            ]
-        };
-    } 
-    
-    render() {
-        return (
-            <div className="Authors">
-                <div className="lander">
-                    <AuthorDisplay authors={this.state.authors} />
-                </div>
+export default function Author(props){
+    const [data, setData] = useState([]);
+    useEffect(() =>{
+        fetch("https://localhost:5001/api/authors/")
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, [])
+    return(
+        <div className="Author">
+            <div className="lander">
+               <AuthorDisplay author={data}/>
             </div>
-        );
-    }
+        </div>
+    );
 }
--- src/books/Books.js
+++ src/books/Books.js
@@ -1,65 +1,19 @@
-import React, { Component } from "react";
+import React, {useEffect, useState} from "react";
 import "./Books.css";
 import { BookDisplay } from "./BookDisplay";
 
-export default class Books extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            books: [
-                {
-                    id: 1,
-                    title: "Domain Driven Design",
-                    author: "Eric Evans",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id: 2,
-                    title: "Accelerate",
-                    author: "Nicole Forsgren",
-                    isbn: "978-1942788331"
-                },
-                {
-                    id: 3,
-                    title: "Cassandra's Story",
-                    author: "Kristyn Burnham",
-                    isbn: "978-1234567890"
-                },
-                {
-                    id: 4,
-                    title: "Henry's Story",
-                    author: "Kristyn Burnham",
-                    isbn: "978-1234567891"
-                },
-                {
-                    id: 5,
-                    title: "Joshua's Story",
-                    author: "Kristyn Burnham",
-                    isbn: "978-1234567892"    
-                },
-                {
-                    id: 6,
-                    title: "Xavier's Story",
-                    author: "Kristyn Burnham",
-                    isbn: "978-1234567893"
-                },
-                {
-                    id: 7,
-                    title: "Last One",
-                    author: "Kristyn Burnham",
-                    isbn: "978-1234567894"    
-                }
-            ]
-        };
-    } 
-    
-    render() {
-        return (
-            <div className="Books">
-                <div className="lander">
-                    <BookDisplay books={this.state.books} />
-                </div>
+export default function Books(props){
+    const [data, setData] = useState([]);
+    useEffect(() =>{
+        fetch("https://localhost:5001/api/books/")
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, [])
+    return(
+        <div className="Books">
+            <div className="lander">
+                <BookDisplay books={data}/>
             </div>
-        );
-    }
+        </div>
+    );
 }
